3cbdb91cf47d07a6b7d7f9414ff9d380a215ea03
Johan <jg2johan@gmail.com>
2023-01-22 22:24:37 +0000

    Add routes and user signup form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   
   root to: "pages#home"
   resources :articles
-  
+  get "signup", to: "users#new"
+  # post "users", to: "users#create"
+  resources :users, except: [:new]
 end
